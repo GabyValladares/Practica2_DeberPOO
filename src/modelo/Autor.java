2b83692b55545975fbc2d2f287b14a3618e6d81e
GabyValladares <gvalladares@ist17dejulio.edu.ec>
2026-06-04 08:33:04 -0500

    Proyecto con modelo Autor

---
+++ src/modelo/Autor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author hp
+ */
+public class Autor {
+    public String nombre;
+    public String apellidos;
+    public int cedula;
+    public String direccion;
+    public String fechaNacimiento;
+    public String nacionalidad;
+       
+    
+}
---
+++ src/modelo/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author hp
+ */
+public class Main {
+    public static void main(String[] args) {
+        //CREAR OBJETO
+        
+        //FORMA 1
+        //nombreClase nombraVariable=new nombreClase();
+        Autor manzana=new Autor();
+        manzana.apellidos="Valladares";
+        
+        //FORMA 2 solo en Clases
+        //Autor tobar;
+        Autor tobar=new Autor();
+        tobar.apellidos="Tobar";
+    }
+    
+}
